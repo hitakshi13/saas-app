8d7ef5a5fb34709d3b7a03b00d2e341ee0c4ed65
Hitakshi Kharag <hitakshikharag@Hitakshis-MacBook-Air.local>
2025-06-08 18:39:22 +0530

    implement navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Bricolage_Grotesque } from "next/font/google";
 import "./globals.css";
+import Navbar from "../components/Navbar";
 
 const bricolage = Bricolage_Grotesque({
   variable: "--font-bricolage",
@@ -13,13 +14,17 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
+                                     children,
+                                   }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${bricolage.variable} antialiased`}>{children}</body>
-    </html>
+      <html lang="en">
+      <body className={`${bricolage.variable} antialiased`}>
+      <Navbar />
+      {children}
+      </body>
+      </html>
   );
 }
+
---
+++ components/Navbar.tsx
@@ -0,0 +1,21 @@
+import NavItems from "@/components/NavItems";
+import Link from 'next/link'
+import Image from 'next/image'
+const Navbar = () => {
+  return (
+<nav className="navbar">
+    <Link href="/">
+        <div className="flex items-center gap-2.5 cursor-pointer">
+         <Image src="/images/logo.svg" alt="logo" width={46} height={44}></Image>
+        </div>
+    </Link>
+    <div className="flex items-center gap-8">
+       <NavItems />
+        <p>Sign In</p>
+
+    </div>
+</nav>
+  )
+}
+
+export default Navbar
